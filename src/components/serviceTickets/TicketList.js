bb6ee4c1d09fb1b7b28ac620789a17205b8a9d74
Scott47 <scottsilver47@gmail.com>
2021-09-05 12:56:54 -0500

    feat: user can delete service ticket

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -7,16 +7,28 @@ export const TicketList = () => {
     const [tickets, setServiceTickets] = useState([])
     const history = useHistory()
 
+    const deleteTicket = (id) => {
+        fetch(`http://localhost:8088/serviceTickets/${id}`, {
+            method: "DELETE"
+        })
+        .then(() => getTickets())
+    }
+
+    const getTickets = () => {
+        return fetch(`${process.env.REACT_APP_BASE_URL}/serviceTickets?_expand=customer&_expand=employee`)
+            .then(res => res.json())
+            .then(
+                (serviceTickets) => setServiceTickets(serviceTickets)
+            )
+    }
+
     useEffect(
         () => {
-            fetch(`${process.env.REACT_APP_BASE_URL}/serviceTickets?_expand=customer&_expand=employee`)
-                .then(res => res.json())
-                .then(
-                    (serviceTickets) => setServiceTickets(serviceTickets)
-                )
+            getTickets()
         },
         []
     )
+
 
     return (
         <>
@@ -28,10 +40,12 @@ export const TicketList = () => {
                 tickets.map(ticket =>
                     <div key={ticket.id}>Issue #: {ticket.id}
                         <p className={ticket.emergency ? "emergency" : "ticket"}>
-                            {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description} submitted by {ticket.customer?.name} 
-                            and worked on by {ticket.employee?.name}</Link>
+                            {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description} submitted by {ticket.customer?.name}
+                                and worked on by {ticket.employee?.name}</Link>
                         </p>
-
+                        <button onClick={() => {
+                            deleteTicket(ticket.id)
+                        }}>Delete</button>
                     </div>
                 )
             }
